712ac302ca3157ccc67ddaf7ed8f8bbe0ed68301
Petr Ankudinov <admin@lab.net>
2026-05-14 13:02:34 +0000

    .

--- containers/lab/.devcontainer/entrypoint.sh
+++ containers/lab/.devcontainer/entrypoint.sh
@@ -5,7 +5,7 @@ set +e
 if [ -z "${CODE_SERVER_BIND_ADDR}" ]; then
     CODE_SERVER_BIND_ADDR="0.0.0.0:5000"
 fi
-code-server --bind-addr ${CODE_SERVER_BIND_ADDR} --auth password --disable-telemetry --disable-update-check --disable-workspace-trust "${CONTAINERWSF}" &
+code-server --bind-addr ${CODE_SERVER_BIND_ADDR} --auth password --disable-telemetry --disable-update-check --disable-workspace-trust "${CONTAINERWSF}" -base-path /inner &
 
 # on Codespaces this will not work correctly
 if ! ${CODESPACES:-false}; then
